8f29e1ca3cadb27c25acba9854f2c80f2234df6c
Raj patel <9795093341raj@gmail.com>
2026-04-01 14:51:38 +0530

    fetched swiggy JSON to dynamically render res cards

--- app.js
+++ app.js
@@ -35,45 +35,799 @@ const Header=()=>{
     )
 }
 
-const styleCard ={
-    backgroundColor: "#f0f0f0",
-}
+const RestaurantCard =(props) => {
+const{resData}=props;
+const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime
+} = resData?.info || {};
+return (
+    <div className="res-card">
+    <img
+        className="res-logo"
+        alt="res-logo"
+        src={
+        "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+        cloudinaryImageId
+        }
+    />
+    <h3>{name}</h3>
+    <h4>{cuisines.join(", ")}</h4>
+    <h4>{avgRating} stars</h4>
+    <h4>{costForTwo}</h4>
+    <h4>{deliveryTime} mins</h4>
+    </div>
+);
+};
 
-const RestaurantCard=()=>{
-    return(
-        <div className="res-card" style={styleCard}>
-            <img  className="res-logo"
-            alt="res-logo"
-            src= "https://static.vecteezy.com/system/resources/previews/040/986/112/non_2x/ai-generated-indian-biryani-rice-professional-advertising-foodgraphy-photo.jpg"
-            />
-            <h3>Four Season</h3>
-            <h4>North Indian ,Biryani ,Asian</h4>
-            <h4>4.2 stars</h4>
-            <h4>28 minutes</h4>
-        </div>
-    )
-}
 
-const Body=()=>{
-    return(
+
+
+const resList={"restaurants":[
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "1037892",
+"name": "Domino's Pizza",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/3/27/a166ceb1-9fd9-4152-93ef-f2307b6781a0_1037892.JPG",
+"locality": "Scheme No. 2",
+"areaName": "SUJATGANJ",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Pizzas",
+"Italian",
+"Pastas",
+"Desserts"
+],
+"avgRating": 4.3,
+"parentId": "2456",
+"avgRatingString": "4.3",
+"totalRatingsString": "497",
+"sla": {
+"deliveryTime": 30,
+"lastMileTravel": 2.4,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "2.4 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-04-01 02:59:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹69"
+},
+"loyaltyDiscoverPresentationInfo": {
+"logoCtx": {
+"text": "BENEFITS",
+"logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+},
+"freedelMessage": "FREE DELIVERY"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-36b492d2-3440-45f8-b2e9-757707af08f5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/kanpur/dominos-pizza-scheme-no-2-sujatganj-rest1037892",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "658632",
+"name": "IGP Cakes",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/3/24/f7833dd1-e2df-47fe-aff7-3a617b5cd9b8_658632.JPG",
+"locality": "Ganga Nagar",
+"areaName": "Lal Bangla",
+"costForTwo": "₹299 for two",
+"cuisines": [
+"Desserts",
+"Bakery"
+],
+"avgRating": 4.5,
+"veg": true,
+"parentId": "392350",
+"avgRatingString": "4.5",
+"totalRatingsString": "2.8K+",
+"sla": {
+"deliveryTime": 51,
+"lastMileTravel": 3,
+"serviceability": "SERVICEABLE",
+"slaString": "50-60 mins",
+"lastMileTravelString": "3.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-04-01 01:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"theme": "light"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹39"
+},
+"loyaltyDiscoverPresentationInfo": {
+"logoCtx": {
+"text": "BENEFITS",
+"logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+},
+"freedelMessage": "FREE DELIVERY"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-36b492d2-3440-45f8-b2e9-757707af08f5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/kanpur/igp-cakes-ganga-nagar-lal-bangla-rest658632",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "1337974",
+"name": "Daily Protein Club",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2026/3/1/f79c4c04-af19-4d18-95b8-00202b7cd99e_1337974.JPG",
+"locality": "Ground floor,12 1",
+"areaName": "Krishna Nagar",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"Kebabs",
+"Fast Food",
+"Snacks",
+"American",
+"Healthy Food",
+"Desserts",
+"Beverages"
+],
+"parentId": "681305",
+"avgRatingString": "NEW",
+"sla": {
+"deliveryTime": 37,
+"lastMileTravel": 4.2,
+"serviceability": "SERVICEABLE",
+"slaString": "35-40 mins",
+"lastMileTravelString": "4.2 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-04-01 02:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"loyaltyDiscoverPresentationInfo": {
+"logoCtx": {
+"text": "BENEFITS",
+"logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+},
+"freedelMessage": "FREE DELIVERY"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"isNewlyOnboarded": true,
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-36b492d2-3440-45f8-b2e9-757707af08f5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/kanpur/daily-protein-club-ground-floor-12-1-krishna-nagar-rest1337974",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "803297",
+"name": "IGP sweets",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/8/3f9fdace-524b-4d6b-9269-20cf8fe49cde_803297.JPG",
+"locality": "Wazidpur",
+"areaName": "Lal Bangla",
+"costForTwo": "₹299 for two",
+"cuisines": [
+"Desserts",
+"Bakery"
+],
+"avgRating": 4.2,
+"veg": true,
+"parentId": "478272",
+"avgRatingString": "4.2",
+"totalRatingsString": "12",
+"sla": {
+"deliveryTime": 49,
+"lastMileTravel": 3,
+"serviceability": "SERVICEABLE",
+"slaString": "45-50 mins",
+"lastMileTravelString": "3.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-04-01 01:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"theme": "light"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹79"
+},
+"loyaltyDiscoverPresentationInfo": {
+"logoCtx": {
+"text": "BENEFITS",
+"logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+},
+"freedelMessage": "FREE DELIVERY"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-36b492d2-3440-45f8-b2e9-757707af08f5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/kanpur/igp-sweets-wazidpur-lal-bangla-rest803297",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "1075978",
+"name": "Whisked Elegance Cake",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/8/714d4b48-7c00-47b3-8770-fea6d2b847f5_1075978.jpg",
+"locality": "Lal Bangla",
+"areaName": "Lal Bangla",
+"costForTwo": "₹300 for two",
+"cuisines": [
+"Cakes & Pastries",
+"Bakery"
+],
+"avgRating": 4.6,
+"veg": true,
+"parentId": "625960",
+"avgRatingString": "4.6",
+"totalRatingsString": "16",
+"sla": {
+"deliveryTime": 48,
+"lastMileTravel": 1.2,
+"serviceability": "SERVICEABLE",
+"slaString": "45-50 mins",
+"lastMileTravelString": "1.2 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-04-01 05:29:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "₹200 OFF",
+"subHeader": "ABOVE ₹999",
+"discountTag": "FLAT DEAL"
+},
+"loyaltyDiscoverPresentationInfo": {
+"logoCtx": {
+"text": "BENEFITS",
+"logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+},
+"freedelMessage": "FREE DELIVERY"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-36b492d2-3440-45f8-b2e9-757707af08f5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/kanpur/whisked-elegance-cake-lal-bangla-rest1075978",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "924409",
+"name": "Pushpa Paratha Center",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/12/b4a8730b-d764-4aba-9989-0200e5476377_924409.JPG",
+"locality": "Savitri Nagar Rd",
+"areaName": "Lal Bangla",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"North Indian"
+],
+"avgRating": 5,
+"veg": true,
+"parentId": "536246",
+"avgRatingString": "5.0",
+"totalRatingsString": "5",
+"sla": {
+"deliveryTime": 52,
+"lastMileTravel": 1.5,
+"serviceability": "SERVICEABLE",
+"slaString": "50-60 mins",
+"lastMileTravelString": "1.5 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-04-01 03:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"theme": "light"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹139"
+},
+"loyaltyDiscoverPresentationInfo": {
+"logoCtx": {
+"text": "BENEFITS",
+"logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+},
+"freedelMessage": "FREE DELIVERY"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-36b492d2-3440-45f8-b2e9-757707af08f5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/kanpur/pushpa-paratha-center-savitri-nagar-rd-lal-bangla-rest924409",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "871084",
+"name": "Anand'S Kitchen",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/15/733b714b-e8d1-426d-af4f-2291136bd899_871084.jpg",
+"locality": "DEVKI NAGAR YASHODA NAGAR",
+"areaName": "YASHODA NAGAR",
+"costForTwo": "₹200 for two",
+"cuisines": [
+"North Indian",
+"South Indian",
+"Chinese",
+"Momos",
+"Pastas",
+"rolls",
+"Snacks",
+"Burgers"
+],
+"avgRating": 4.2,
+"parentId": "333746",
+"avgRatingString": "4.2",
+"totalRatingsString": "42",
+"sla": {
+"deliveryTime": 58,
+"lastMileTravel": 3,
+"serviceability": "SERVICEABLE",
+"slaString": "55-65 mins",
+"lastMileTravelString": "3.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-04-01 23:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"description": "pureveg"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "pureveg",
+"imageId": "v1695133679/badges/Pure_Veg111.png",
+"theme": "light"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"loyaltyDiscoverPresentationInfo": {
+"logoCtx": {
+"text": "BENEFITS",
+"logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+},
+"freedelMessage": "FREE DELIVERY"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-36b492d2-3440-45f8-b2e9-757707af08f5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/kanpur/anands-kitchen-devki-nagar-yashoda-nagar-rest871084",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "1196146",
+"name": "Ziddi Mugra",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/5/2b910991-0314-4107-9c5f-a9c76ebb2771_1196146.jpg",
+"locality": "Chawla Market",
+"areaName": "Chawla Market",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Fast Food",
+"Burgers",
+"North Indian"
+],
+"parentId": "683802",
+"avgRatingString": "--",
+"sla": {
+"deliveryTime": 56,
+"lastMileTravel": 2.7,
+"serviceability": "SERVICEABLE",
+"slaString": "55-65 mins",
+"lastMileTravelString": "2.7 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-04-01 04:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"loyaltyDiscoverPresentationInfo": {
+"logoCtx": {
+"text": "BENEFITS",
+"logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+},
+"freedelMessage": "FREE DELIVERY"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-36b492d2-3440-45f8-b2e9-757707af08f5"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/kanpur/ziddi-mugra-chawla-market-rest1196146",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+]}
+;
+
+const Body = () => {
+    return (
         <div className="body">
-            <div className="search">
-                Search
-            </div>
+            <div className="search">Search</div>
             <div className="res-container">
-
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-
+                {resList.restaurants.map((resto) => (
+                    <RestaurantCard key={resto.info.id} resData={resto} />
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
 const Applayout=()=>{
     return(
